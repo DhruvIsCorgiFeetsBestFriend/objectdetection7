0913d0125f8a8d5d079c30d84253737781a9f5f9
Mia Amor Martinez <80438005+DhruvIsCorgiFeetsBestFriend@users.noreply.github.com>
2021-10-01 20:10:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+img = "";
+Status = "";
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#000000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#000000");
+    rect(30 ,60, 450, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+
+    fill("#000000");
+    text("Bowl", 270, 290);
+    noFill();
+    stroke("#000000");
+    rect(260 ,270, 150, 150);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+       console.log(error);
+    }
+    console.log(results);
+}
+
